18eeef5f2c1257f0ad3dd2357441a16fae57e42f
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-09 11:49:20 +0530

    created Js file

---
+++ JAVASCRIPT/1_intro/intro.js
@@ -0,0 +1,24 @@
+// outputs menthod
+console.log("I am External javaScript file");
+// alert("This is alert message")
+document.write("This is document")
+
+//! How to declare the variable in javaScript:
+//1.var
+var num1=20;
+console.log(num1);
+var num2=40;
+console.log(num2);
+
+//2.let
+let num3;
+num3=80;
+console.log(num3);p
+
+let num4=78;
+// let num3=45;
+
+//3/const
+const num6=90;
+console.log(num6);
+
---
+++ JAVASCRIPT/2_dataTypes/dataTypes.js
@@ -0,0 +1,54 @@
+ //! DataTypes in JavaScript
+
+ //! 1.primitive DataTypes
+ //? i) Number Datatypes
+ let a=10;
+ console.log(a);
+ console.log(typeof a); //number
+let b=18.9;
+console.log(b);
+
+console.log(typeof b); //number
+
+let c=Number(50);
+console.log(c);
+console.log(typeof c);
+
+
+//! 2.boolean
+
+let isplayer=true;
+console.log(isplayer);
+console.log(typeof isplayer);
+
+let isStudent=false
+console.log(isStudent);
+console.log(typeof isStudent);
+
+//! 3.String
+//?we can declare String two ways
+//? i) by using literal
+//? ii) by using Object
+
+//? i) by using literal
+let str1="hello";
+console.log(str1);
+console.log(typeof str1);
+
+let str2='hi';
+console.log(str2);
+console.log(typeof str2);
+
+let str3=`bye`
+console.log(str3);
+console.log(typeof str3);
+
+//? ii) by using object
+let strOb=new String('hello')
+console.log(strOb);
+console.log(typeof strOb);
+
+
+
+
+
