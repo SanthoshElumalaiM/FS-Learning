1aedb76f3d9cbd5980a113d117517678dde122cc
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-11 10:42:25 +0530

    String

---
+++ JAVASCRIPT/3_String/String.js
@@ -0,0 +1,19 @@
+//! How to declare String
+//? By using literal
+let str1="hello"
+let str2=`HELLO`
+let str3 ='Bye'
+
+//! How to know the length
+//* length is property not a method,it is used to finding the the length
+let length=str1.length
+console.log(length)
+
+//! 1.toUpperCase()
+//*it is used to convert to upperCase,it returns new String and also using varible we have to store.
+let upper=str1.toUpperCase()
+console.log(str1);
+console.log(upper);
+
+
+
