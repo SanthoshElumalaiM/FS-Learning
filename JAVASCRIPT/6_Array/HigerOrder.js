ef1897ce0687d66d0eb0afc6b939b404e3d912d3
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-17 10:46:37 +0530

    higher_ order

--- JAVASCRIPT/6_Array/HigerOrder.js
+++ JAVASCRIPT/6_Array/HigerOrder.js
@@ -1,6 +1,18 @@
 //! higher order array methods
 let arr=[10,20,30,40,50]
-arr.forEach((ele)=>{
-console.log(ele);
+//! forEach
+//* it can take three parameter > element,index,array
+arr.forEach((ele,index,arr)=>{
+console.log(ele,index,arr);
 
+})
+
+//! how to find the   greatest element  of the given array
+let arr1=[]
+arr.forEach((ele)=>{
+    if(ele>30)
+    {
+        arr1.push(ele)
+    }
+    console.log(arr1);
 })
