e9f3d4c94d4c740887377b957f96b8b249188eb2
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-17 11:17:50 +0530

    higher_ order

--- JAVASCRIPT/6_Array/HigerOrder.js
+++ JAVASCRIPT/6_Array/HigerOrder.js
@@ -2,6 +2,7 @@
 let arr=[10,20,30,40,50]
 //! forEach
 //* it can take three parameter > element,index,array
+//* forEach can't return anything ,it is only use traversing the array
 arr.forEach((ele,index,arr)=>{
 console.log(ele,index,arr);
 
@@ -16,3 +17,43 @@ arr.forEach((ele)=>{
     }
     console.log(arr1);
 })
+//! add 100 with all the element and give the array
+
+let add=[]
+arr.forEach((ele)=>{
+    add.push(ele+100)
+}
+)
+console.log(add);
+
+let sum=0
+arr.forEach((ele)=>{
+    sum+=ele
+})
+console.log(sum);
+console.log("==========================");
+
+
+//! 2.filter method
+//* filter can take also three parameter
+//* if we want to filter the condition we go for filter method
+
+let filterArr=arr.filter((ele)=>{
+    return ele>30
+})
+console.log("using filter method:"+filterArr);
+
+//! in one line
+let filterArr1=arr.filter((ele)=>ele>30)
+console.log("filter in single line:"+filterArr1);
+
+//! 3.Map 
+//* it is used also traversing the array and along with if we want to any arithematic operation it can to do.
+//* it can take three parameter also
+let arrMap=arr.map((ele)=>{
+    return ele+100
+})
+console.log("Using Map method:"+arrMap);
+
+let arrMap1=arr.map((ele)=>ele+100)
+console.log("Map in single line:"+arrMap1);
