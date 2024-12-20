f7be5dc6865eecc3f986c36e9935c9a71e063591
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-20 10:40:11 +0530

    modified for destructure

--- JAVASCRIPT/10_Destructure/destructure.js
+++ JAVASCRIPT/10_Destructure/destructure.js
@@ -24,3 +24,24 @@ console.log(b);
 console.log(c);
 
 
+//! rest Parameter()
+//* it is used to rest of the values are stored in last element only.
+//* we can't stored the first element
+let arr1=[10,20,30,40,50]
+let[d,e,...f]=arr1
+
+console.log(d);
+console.log(e);
+
+console.log(f);
+
+//! spread operator()
+//* it is used to combined the two array without using concat()
+
+let arr2=[99,89]
+let combinedarr=[...arr1,...arr2]
+console.log(combinedarr);
+
+
+
+
