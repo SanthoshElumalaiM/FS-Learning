9832ef9976680c2239566bd9b2497a6c0435b985
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-28 11:48:12 +0530

    EventTask

--- JAVASCRIPT/Task/EventTask/Event.js
+++ JAVASCRIPT/Task/EventTask/Event.js
@@ -13,7 +13,7 @@ div.classList.add("con3")
 let con2=document.querySelector(".container2")
 con2.after(div)
 let button1=document.createElement("button")
-button1.classList.add("btn1")
+button1.classList.toggle("btn1")
 console.log(button1);
 
 let d=document.querySelector(".con3")
@@ -23,3 +23,29 @@ d.after(button1)
 button1.addEventListener("click",()=>{
     d.style.backgroundColor="red"
 })
+
+let add=()=>
+{
+    let num1=parseInt(document.getElementById("num1").value)
+    let num2=parseInt(document.getElementById("num2").value)
+    
+    let sum=num1+num2
+    // console.log(sum);
+    // let res=document.querySelector(".result")
+    let table=document.querySelector("table")
+    let tr=document.createElement("tr")
+    tr.innerHTML=`<td>${num1}</td>
+    <td>${num1}</td>
+    <td>${sum}</td>`
+    table.append(tr)
+    
+
+    
+    
+    
+    // res.innerHTML=`<h1> ${sum} </h1>`;
+    
+    
+    
+}
+
