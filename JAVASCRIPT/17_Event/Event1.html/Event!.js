dca830c691c318d42865af0ad900a76f14538034
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-30 10:44:33 +0530

    Event1

---
+++ JAVASCRIPT/17_Event/Event1.html/Event!.js
@@ -0,0 +1,12 @@
+
+let btn=document.getElementById("btn");
+// console.log(btn);
+btn.addEventListener("click",()=>{
+    console.log("am eventListener");
+    
+})
+btn.addEventListener("click",()=>{
+    console.log("SeccondTime EventListener");
+    
+})
+
